dfba2ced189f148cbe141c5d44298a44683cacde
Vyachesl-ux <65283740+Vyachesl-ux@users.noreply.github.com>
2021-02-16 17:50:26 +0100

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,79 @@
+                /* Filter
+                                                                ===============*/
+                $(function () {
+                    let filter = $("[data-filter]");
+                    filter.on("click", function (event) {
+                        event.preventDefault();
+                        let cat = $(this).data('filter');
+                        if (cat == 'all') {
+                            $("[data-category]").removeClass('hide');
+                        }
+                        else {
+                            $("[data-category]").each(function () {
+                                let workCat = $(this).data('category');
+                                if (workCat != cat) {
+                                    $(this).addClass('hide');
+                                }
+                                else {
+                                    $(this).removeClass('hide');
+                                }
+                            });
+                        }
+                    });
+                    /* MODAL
+                    ===============*/
+                    const modalCall = $('[data-modal]');
+                    const modalClose = $('[data-close]');
+                    modalCall.on("click", function (event) {
+                        event.preventDefault();
+                        let $this = $(this);
+                        let modalId = $this.data('modal');
+                        $(modalId).addClass('show');
+                        $("body").addClass('no-scroll');
+                        setTimeout(function () {
+                            $(modalId).find(".modal__dialog").css({
+                                transform: "rotateX(0)"
+                            });
+                        }, 200);
+                        $('#worksSlide').slick('setPosition');
+                    });
+                    modalClose.on("click", function (event) {
+                        event.preventDefault();
+                        let $this = $(this);
+                        let modalParent = $this.parents('.modal');
+                        modalParent.find(".modal__dialog").css({
+                            transform: "rotateX(90deg)"
+                        });
+                        setTimeout(function () {
+                            modalParent.removeClass('show');
+                            $("body").removeClass('no-scroll');
+                        }, 200);
+                    });
+                    $(".modal").on("click", function (event) {
+                        $(this).removeClass('show');
+                        $("body").removeClass('no-scroll');
+                    });
+                    $(".modal__dialog").on("click", function (event) {
+                        event.stopPropagation();
+                    });
+                });
+                /* Slider: http://kenwheeler.github.io/slick/
+                =========*/
+                $('[data-slider="slick"]').slick({
+                    infinite: true
+                    , slidesToShow: 1
+                    , slidesToScroll: 1
+                    , fade: true
+                    , arrows: false
+                    , dots: true
+                });
+                $(".slickPrev").on("click", function (event) {
+                    event.preventDefault();
+                    let currentSlider = $(this).parents('.modal').find('[data-slider="slick"]');
+                    currentSlider.slick("slickPrev");
+                });
+                $(".slickNext").on("click", function (event) {
+                    event.preventDefault();
+                    let currentSlider = $(this).parents('.modal').find('[data-slider="slick"]');
+                    currentSlider.slick("slickNext");
+                });
